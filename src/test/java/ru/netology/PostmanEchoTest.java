422d3d3156d355977570586bb130b2d4c3d7d0e9
Yaroshenko Natalya <natavik2401@gmail.com>
2025-03-05 01:36:48 +0300

    Change commit1

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
              .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет, мир!"));
+                .body("data", equalTo("Привет, Mир!"));
     }
 }
 
