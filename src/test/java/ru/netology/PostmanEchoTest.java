74eb1d9cb5f5261e3ee0b9e3f7c11ca20568c1b2
Yaroshenko Natalya <natavik2401@gmail.com>
2025-03-05 02:09:22 +0300

    Change commit3

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -8,19 +8,18 @@ import static org.hamcrest.Matchers.equalTo;
 public class PostmanEchoTest {
 
     @Test
-
-    void shouldReturnSendData () {
+    void shouldReturnSendBody() {
         given()
-                .baseUri ("https://postman-echo.com")
+                .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Привет, мир!")
-             .when()
+                .body("Привет. мир!")
+        .when()
                 .post("/post")
-             .then()
+        .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет, мир!"));
+                .body("data", equalTo("Привет. мир!"));
+
     }
 }
 
 
-// String text= "Some data";
